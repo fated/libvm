d650d49eed223e21cd330abab3f2d95ac31827d7
fated <brucechou24@gmail.com>
2014-05-20 08:57:52 +0100

    simple working 1NN added

--- vm-offline.cpp
+++ vm-offline.cpp
@@ -1,11 +1,70 @@
 #include "utilities.h"
+#include <iostream>
 #include <string>
+#include <cmath>
+
+double KNN(struct Problem *train, struct Node *x);
+double CalcDist(struct Node *x1, struct Node *x2);
 
 int main(int argc, char *argv[])
 {
-  char *input_file_name = argv[1];
-  struct Problem *prob;
+  char *train_file_name = argv[1];
+  char *test_file_name = argv[2];
+  struct Problem *train, *test;
+  int error = 0;
 
-  prob = ReadProblem(input_file_name);
+  train = ReadProblem(train_file_name);
+  test = ReadProblem(test_file_name);
+
+  for (int i = 0; i < test->l; ++i) {
+    double predict_label;
+
+    predict_label = KNN(train, test->x[i]);
+    if (predict_label != test->y[i]) {
+      ++error;
+    }
+  }
+
+  std::cout << error << '/' << test->l << std::endl;
+
   return 0;
+}
+
+double KNN(struct Problem *train, struct Node *x)
+{
+  int l = train->l, index = -1;
+  double min_d = -1;
+
+  for (int i = 0; i < l; ++i) {
+    double d = CalcDist(train->x[i], x);
+    if (d < min_d || min_d == -1) {
+      min_d = d;
+      index = i;
+    }
+  }
+
+  return train->y[index];
+}
+
+double CalcDist(struct Node *x1, struct Node *x2)
+{
+  double sum = 0;
+
+  while (x1->index != -1 && x2->index != -1) {
+    if (x1->index == x2->index) {
+      sum += (x1->value - x2->value) * (x1->value - x2->value);
+      ++x1;
+      ++x2;
+    } else {
+      if(x1->index > x2->index) {
+        sum += x2->value * x2->value;
+        ++x2;
+      } else {
+        sum += x1->value * x1->value;
+        ++x1;
+      }
+    }
+  }
+
+  return sqrt(sum);
 }

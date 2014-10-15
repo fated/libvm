cb9b7b9f2c9b9f2779a0c97ae86c1388f9b2412b
fated <brucechou24@gmail.com>
2014-10-15 23:25:56 +0100

    add raw Spoc functions

--- mcsvm.cpp
+++ mcsvm.cpp
@@ -16,61 +16,6 @@ int CompareNodes(const void *n1, const void *n2) {
     return (0);
 }
 
-
-static MCDataUnit **x   = NULL;
-static long *y       = NULL;
-static long m        = 0;
-static long k        = 0;
-static long l        = 0;
-static double **tau = NULL;
-static double beta  = 0;
-
-static SPOCParamDef spoc_pd;
-static MCDataDef mc_datadef;
-static MCSolution mc_sol;
-static RedOptDef redopt_def;
-static REDOPT_FUN redopt_fun;
-static KF kernel_function;
-
-static double *vector_a            = NULL;
-static double **matrix_f           = NULL;
-static double *row_matrix_f_next_p = NULL;
-static long    **matrix_eye         = NULL;
-static double *delta_tau           = NULL;
-static double *old_tau             = NULL;
-static double *vector_b            = NULL;
-
-static double max_psi              =0;
-static long    next_p;
-static long    next_p_list;
-
-static long    *supp_pattern_list = NULL;
-static long    *zero_pattern_list = NULL;
-static long    n_supp_pattern     = 0;
-static long    n_zero_pattern     = 0;
-
-
-int    longcmp(const void *n1, const void *n2);
-void   spoc_construct();
-long   spoc_epsilon(double epsilon);
-void   choose_next_pattern(long *pattern_list, long n_pattern);
-void   update_matrix_f(double *kernel_pattern_p);
-long   allocate_memory();
-void   free_auxilary_memory();
-void   free_external_memory();
-void   spoc_initialize();
-void   update_matrix_f(double *kernel_next_p);
-double next_epsilon1(double epsilon_cur, double epsilon);
-double next_epsilon2(double epsilon_cur, double epsilon);
-long   get_no_supp1();
-double get_train_error(double b);
-void   dump_matrix_f();
-
-int   intcmp(const void *n1, const void *n2) {
-  return (*(int*)(n1) - *(int*)(n2));
-}
-
-
 //
 // Kernel Cache
 //
@@ -641,11 +586,77 @@ int RedOpt::GetMarginError(const double beta) {
   return errors;
 }
 
+// RedOpt start
 
+// Spoc start
 
+class Spoc {
+ public:
+  Spoc() {};
+  virtual ~Spoc() {};
 
-// calculate kernels
-void spoc_initialize() {
+  struct SolutionInfo {
+    double obj;
+    double rho;
+    double upper_bound_p;
+    double upper_bound_n;
+    double r;  // for Solver_NU
+  };
+
+  void Solve();
+
+ protected:
+
+ private:
+  int cache_size_;
+  double beta;
+  double epsilon;
+  double epsilon0;
+  double delta;
+};
+
+Spoc::Spoc(MCSVMParameter &param) {
+  long req_n_blocks_lrucache;
+  long n_blocks_lrucache;
+
+  Info("\nOptimizer (SPOC)  (version 1.0)\n");
+  Info("Initializing ... start \n");
+
+  x = mc_datadef.x;
+  y = mc_datadef.y;
+  m = mc_datadef.m;
+  k = mc_datadef.k;
+  l = mc_datadef.l;
+
+  /* scale beta with m */
+  fflush(stdout);
+  beta = spoc_pd.beta;
+
+  printf("Requested margin (beta) %e\n", spoc_pd.beta);
+  kernel_function = kernel_get_function(spoc_pd.kernel_def);
+
+  allocate_memory();
+  redopt_construct(k);
+  kernel_construct(l);
+
+  n_supp_pattern = 0;
+  n_zero_pattern = 0;
+
+  req_n_blocks_lrucache = MIN(m, ((long)(((double)spoc_pd.cache_size) / ((((double)sizeof(double)) * ((double)m)) / ((double)MB)))));
+  printf("Requesting %ld blocks of cache (External bound %ldMb)\n",
+   req_n_blocks_lrucache, spoc_pd.cache_size);
+  n_blocks_lrucache = cachelru_construct(m, req_n_blocks_lrucache, sizeof(double)*m);
+
+  spoc_initialize();
+
+  printf("Initializing ... done\n"); fflush(stdout);
+}
+
+Spoc::~Spoc() {
+
+}
+
+void Spoc::spoc_initialize() {
 
   long i;
   long s,r;
@@ -678,32 +689,6 @@ void spoc_initialize() {
     for (r=0 ;r<k; r++)
       tau[i][r] = 0;
 
-/*  assume tau=0 */
-/*  ============ */
-/*    for (j=0; j<m; j++) { */
-/*      for (r=0 ;r<k; r++) { */
-/*        if (tau[j][r] != 0) { */
-/*    for (i=0; i<m; i++) */
-/*      matrix_f[i][r] += kernel_function(x[i], x[j]) * tau[j][r]; */
-/*        } */
-/*      } */
-/*    } */
-
-/*    for (i=0; i<m; i++) { */
-/*      is_tau_r_zero = 1; */
-
-/*      for (r=0; r<k; r++) { */
-/*        if (tau[i][r] != 0) { */
-/*    is_tau_r_zero = 0; */
-/*    break; */
-/*        } */
-/*      } */
-/*      if (is_tau_r_zero) */
-/*        zero_pattern_list[n_zero_pattern++] =i; */
-/*      else */
-/*        supp_pattern_list[n_supp_pattern++] =i; */
-/*    } */
-
   supp_pattern_list[0] =0;
   n_supp_pattern =1;
 
@@ -713,96 +698,175 @@ void spoc_initialize() {
   choose_next_pattern(supp_pattern_list, n_supp_pattern);
 }
 
-long allocate_memory() {
+long Spoc::spoc_epsilon(double epsilon) {
+
+  long supp_only =1;
+  long cont = 1;
+  long mistake_k;
+  double *kernel_next_p;
+  long r;
   long i;
 
-  /* tau */
-  tau = (double **) ut_calloc(m, sizeof(double*));
-  *tau = (double *) ut_calloc(m*k, sizeof(double));
-  for (i=1; i<m; i++)
-    tau[i] = tau[i-1] + k;
+  while (cont) {
+    max_psi = 0;
+    if (supp_only)
+      choose_next_pattern(supp_pattern_list, n_supp_pattern);
+    else
+      choose_next_pattern(zero_pattern_list, n_zero_pattern);
 
-  /* vector_a */
-  vector_a = (double *) ut_calloc(m, sizeof(double));
+    if (max_psi > epsilon * beta) {
+      redopt_def.a = vector_a[next_p];
+      for (r=0; r<k; r++)
+        redopt_def.b[r] = matrix_f[next_p][r] - redopt_def.a * tau[next_p][r];
+      redopt_def.y = y[next_p];
+      for (r=0; r<k; r++)
+        old_tau[r] = tau[next_p][r];
+      redopt_def.alpha = tau[next_p];
 
-  /* matrix_f */
-  matrix_f = (double **) ut_calloc(m, sizeof(double*));
-  *matrix_f = (double *) ut_calloc(m*k, sizeof(double));
-  for (i=1; i<m; i++)
-    matrix_f[i] = matrix_f[i-1] + k;
+      mistake_k = (*redopt_fun)(&redopt_def);
 
-  /* matrix_eye */
-  matrix_eye = (long **) ut_calloc(k, sizeof(long*));
-  *matrix_eye = (long *) ut_calloc(k*k, sizeof(long));
-  for (i=1; i<k; i++)
-    matrix_eye[i] = matrix_eye[i-1] + k;
+      for (r=0; r<k; r++)
+        delta_tau[r] = tau[next_p][r]- old_tau[r];
 
-  /* delta_tau */
-  delta_tau = (double *) ut_calloc(k, sizeof(double));
+      if (!cachelru_retrive(next_p, (void*)(&kernel_next_p))) {
+        for (i=0; i<m; i++)
+          kernel_next_p[i] = kernel_function(x[i], x[next_p]);
+      }
 
-  /* old_tau */
-  old_tau = (double *) ut_calloc(k, sizeof(double));
+      update_matrix_f(kernel_next_p);
 
-  /* vector_b */
-  vector_b = (double *) ut_calloc(k, sizeof(double));
+      if (supp_only) {
+        for (r=0; r<k; r++)
+          if (tau[next_p][r] != 0) break;
+        if (r == k) {
+          zero_pattern_list[n_zero_pattern++] = next_p;
+          supp_pattern_list[next_p_list] = supp_pattern_list[--n_supp_pattern];
+        }
+      } else {
+        supp_pattern_list[n_supp_pattern++] = next_p;
+        zero_pattern_list[next_p_list] = zero_pattern_list[--n_zero_pattern];
+        supp_only =1;
+      }
+    } else {
+      if (supp_only)
+        supp_only =0;
+      else
+        cont =0;
+    }
+  }
 
-  /* supp_pattern_list */
-  supp_pattern_list = (long *) ut_calloc(m, sizeof(long));
+  return (1);
+}
 
-  /* zero_pattern_list */
-  zero_pattern_list = (long *) ut_calloc(m, sizeof(long));
+double Spoc::next_epsilon1(double epsilon_cur, double epsilon) {
 
- return (1);
+  double e;
+  e= epsilon_cur; /* just for the compiler */
+  e=  ((max_psi / beta) * .95);
 
+  return (MAX(e, epsilon));
+}
+
+double Spoc::next_epsilon2(double epsilon_cur, double epsilon) {
+  static double iteration =12;
+  double e = epsilon_cur / log10(iteration);
+
+  iteration+=2;
+
+  return (MAX( e , epsilon));
+}
+
+void Spoc::choose_next_pattern(long *pattern_list, long n_pattern) {
+
+  double psi;    /* KKT value of example */
+  double psi1;   /* max_r matrix_f[i][r] */
+  double psi0;   /* min_{r, tau[i][r]<delta[yi][r]}  matrix_f[i][r] */
+
+  long i;
+  long r;
+  long p=0;
+
+  double *matrix_f_ptr;
+  for (i=0; i<n_pattern; i++) {
+    psi1 = -DBL_MAX;
+    psi0 =  DBL_MAX;
+
+    p=pattern_list[i];
+    matrix_f_ptr = matrix_f[p];
+
+    for (r=0; r<k; r++, matrix_f_ptr++) {
+      if (*matrix_f_ptr > psi1)
+  psi1 = *matrix_f_ptr;
+
+      if (*matrix_f_ptr < psi0)
+  if (tau[p][r] < matrix_eye[y[p]][r])
+    psi0 = *matrix_f_ptr;
+    }
+
+    psi = psi1 - psi0;
+
+    if (psi > max_psi) {
+      next_p_list = i;
+      max_psi = psi;
+    }
+  }
+  next_p = pattern_list[next_p_list];
+  row_matrix_f_next_p = matrix_f[p];
+}
+
+
+void Spoc::update_matrix_f(double *kernel_next_p) {
+
+  long i;
+  long r;
+
+  double *delta_tau_ptr = delta_tau;
+  double *kernel_next_p_ptr;
+
+  for (r=0; r<k; r++, delta_tau_ptr++)
+    if (*delta_tau_ptr != 0)
+      for (i=0, kernel_next_p_ptr = kernel_next_p ; i<m; i++, kernel_next_p_ptr++)
+  matrix_f[i][r] += (*delta_tau_ptr) * (*kernel_next_p_ptr);
+}
+
+double Spoc::get_train_error(double b) {
+  long i, r;
+  double max;
+  double errors = 0;
+  for (i=0; i<m; i++) {
+    max = -DBL_MAX;
+    for (r=0; r<y[i]; r++) {
+      if (matrix_f[i][r]>max) {
+  max = matrix_f[i][r];
+      }
+    }
+    for (r++; r<k; r++) {
+      if (matrix_f[i][r]>max) {
+  max = matrix_f[i][r];
+      }
+    }
+    if ((max-b) >= matrix_f[i][y[i]])
+      errors += 1;
+  }
+  return (100*errors/((double)m));
+}
+
+long Spoc::get_no_supp1() {
+  long n =0;
+  long i;
+
+  for (i=0; i<m; i++)
+    if (tau[i][y[i]] == 1)
+      n++;
+  return (n);
 }
 
 
 
-
-
-
+// Spoc class end
 
 MCSVMModel *TrainMCSVM(const struct Problem *prob, const struct MCSVMParameter *param) {
   double epsilon_current = 1;
-
-  // spoc_construct()
-  long req_n_blocks_lrucache;
-  long n_blocks_lrucache;
-
-  printf("\nOptimizer (SPOC)  (version 1.0)\n");
-  printf("Initializing ... start \n"); fflush(stdout);
-
-  x = prob.x;
-  y = prob.y;
-  m = prob.m;
-  k = prob.k;
-  l = prob.l;
-
-  /* scale beta with m */
-  fflush(stdout);
-  beta = param->beta;
-
-  printf("Requested margin (beta) %e\n", param->beta);
-  kernel_function = kernel_get_function(param->kernel_def);
-
-  allocate_memory();
-  redopt_construct(k);
-  kernel_construct(l);
-
-  n_supp_pattern = 0;
-  n_zero_pattern = 0;
-
-  req_n_blocks_lrucache = MIN(m, ((long)(((double)param->cache_size) / ((((double)sizeof(double)) * ((double)m)) / ((double)MB)))));
-  printf("Requesting %ld blocks of cache (External bound %ldMb)\n",
-   req_n_blocks_lrucache, param->cache_size);
-  n_blocks_lrucache = cachelru_construct(m, req_n_blocks_lrucache, sizeof(double)*m);
-
-  spoc_initialize();
-
-  printf("Initializing ... done\n"); fflush(stdout);
-
-  // spoc_construct() end
-
 
   printf("Epsilon decreasing from %e to %e\n", param->epsilon0, param->epsilon);
   redopt_construct(k);
@@ -865,77 +929,6 @@ MCSVMModel *TrainMCSVM(const struct Problem *prob, const struct MCSVMParameter *
 
   return (mc_sol);
 }
-
-long spoc_epsilon(double epsilon) {
-
-  long supp_only =1;
-  long cont = 1;
-  long mistake_k;
-  double *kernel_next_p;
-  long r;
-  long i;
-
-  while (cont) {
-    max_psi = 0;
-    if (supp_only)
-      choose_next_pattern(supp_pattern_list, n_supp_pattern);
-    else
-      choose_next_pattern(zero_pattern_list, n_zero_pattern);
-
-    if (max_psi > epsilon * beta) {
-      redopt_def.a = vector_a[next_p];
-      for (r=0; r<k; r++)
-        redopt_def.b[r] = matrix_f[next_p][r] - redopt_def.a * tau[next_p][r];
-      redopt_def.y = y[next_p];
-      for (r=0; r<k; r++)
-        old_tau[r] = tau[next_p][r];
-      redopt_def.alpha = tau[next_p];
-
-      mistake_k = (*redopt_fun)(&redopt_def);
-
-      for (r=0; r<k; r++)
-        delta_tau[r] = tau[next_p][r]- old_tau[r];
-
-      if (!cachelru_retrive(next_p, (void*)(&kernel_next_p))) {
-        for (i=0; i<m; i++)
-          kernel_next_p[i] = kernel_function(x[i], x[next_p]);
-      }
-
-      update_matrix_f(kernel_next_p);
-
-      if (supp_only) {
-        for (r=0; r<k; r++)
-          if (tau[next_p][r] != 0) break;
-        if (r == k) {
-          zero_pattern_list[n_zero_pattern++] = next_p;
-          supp_pattern_list[next_p_list] = supp_pattern_list[--n_supp_pattern];
-        }
-      } else {
-        supp_pattern_list[n_supp_pattern++] = next_p;
-        zero_pattern_list[next_p_list] = zero_pattern_list[--n_zero_pattern];
-        supp_only =1;
-      }
-    } else {
-      if (supp_only)
-        supp_only =0;
-      else
-        cont =0;
-    }
-  }
-
-  return (1);
-}
-
-double next_epsilon2(double epsilon_cur, double epsilon) {
-  static double iteration =12;
-  double e = epsilon_cur / log10(iteration);
-
-  iteration+=2;
-
-  return (MAX( e , epsilon));
-}
-
-
 
 double PredictMCSVM(const struct MCSVMModel *model, const struct Node *x) {
 
